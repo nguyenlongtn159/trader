63b9929064851d147aa1dada8295e8293c356888
lhung <lhung@redseal.net>
2020-11-10 09:28:29 +0700

    structure modules

---
+++ web/src/main/java/longnd/web/controllers/HelloController.java
@@ -0,0 +1,6 @@
+package longnd.web.controllers;
+
+public class HelloController {
+    public void Gretting() {}
+
+}
---
+++ web/src/test/java/controller/TestController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestController {
+
+    @Test
+    public void testHello() {
+        Assert.assertTrue(true);
+    }
+
+}
