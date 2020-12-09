16e250f294e082bedd4e43d55a767a9fb2d86c1e
lhung <lhung@redseal.net>
2020-12-09 15:57:44 +0700

    remember-me

--- web/src/main/java/com/longnd/config/WebSecurityConfig.java
+++ web/src/main/java/com/longnd/config/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package com.longnd.config;
 
+import com.longnd.service.UserServiceImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final Log logger = LogFactory.getLog(WebSecurityConfig.class);
+
+    @Autowired
+    UserServiceImpl userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,6 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                     .loginPage("/login").permitAll().defaultSuccessUrl("/", true)
                     .and()
+                .rememberMe()
+                    .key("Trader-rememberMe")
+                    .userDetailsService(userService)
+                .tokenRepository(new InMemoryTokenRepositoryImpl())
+                    .tokenValiditySeconds(1296000)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/resources/**").permitAll()
                 .antMatchers("/logout").permitAll()
